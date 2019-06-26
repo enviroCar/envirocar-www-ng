e4cba6cdaf91f7499f77145fc5a609262e12a806
Christian Autermann <c.autermann@52north.org>
2019-06-26 16:02:06 +0200

    formatting

--- app/app.js
+++ app/app.js
@@ -1,231 +1,300 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    angular.module('enviroCar', [
-        'ngMaterial',
-        'ui-leaflet',
-        'ngAnimate',
-        'ngCookies',
-        'ui.router',
-        'pascalprecht.translate',
-        'translations',
-        'nvd3',
-        'enviroCar.api',
-        'enviroCar.auth',
-        'enviroCar.track',
-        'enviroCar.tracks',
-        'enviroCar.profile',
-        'enviroCar.community',
-        'enviroCar.feedback',
-        'materialCalendar',
-        'cl.paging',
-        'rzModule'])
-            .run(function ($rootScope, $state, $stateParams, $http, $cookies, UserCredentialsService) {
-                "ngInject";
-                console.log('app started');
-                $rootScope.previewurl = "";
-                $rootScope.$state = $state;
-                $rootScope.$stateParams = $stateParams;
+  angular
+    .module("enviroCar", [
+      "ngMaterial",
+      "ui-leaflet",
+      "ngAnimate",
+      "ngCookies",
+      "ui.router",
+      "pascalprecht.translate",
+      "translations",
+      "nvd3",
+      "enviroCar.api",
+      "enviroCar.auth",
+      "enviroCar.track",
+      "enviroCar.tracks",
+      "enviroCar.profile",
+      "enviroCar.community",
+      "enviroCar.feedback",
+      "materialCalendar",
+      "cl.paging",
+      "rzModule"
+    ])
+    .run(function(
+      $rootScope,
+      $state,
+      $stateParams,
+      $http,
+      $cookies,
+      UserCredentialsService
+    ) {
+      "ngInject";
+      console.log("app started");
+      $rootScope.previewurl = "";
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
 
-                $rootScope.$on("$stateChangeStart", function (event, toState, toParams,
-                        fromState, fromParams) {
+      $rootScope.$on("$stateChangeStart", function(
+        event,
+        toState,
+        toParams,
+        fromState,
+        fromParams
+      ) {
+        var credits = UserCredentialsService.getCredentials();
+        if (toState.authenticate && !credits.username) {
+          $state.transitionTo("login");
+          event.preventDefault();
+        }
+      });
 
-                    var credits = UserCredentialsService.getCredentials();
-                    if (toState.authenticate && (!credits.username)) {
-                        $state.transitionTo("login");
-                        event.preventDefault();
-                    }
+      $http.defaults.headers.post["X-CSRFToken"] = $cookies.csrftoken;
+    });
+
+  angular.module("enviroCar.api", []);
+  angular.module("enviroCar.auth", []);
+  angular.module("enviroCar.track", []);
+  angular.module("enviroCar.tracks", []);
+  angular.module("enviroCar.profile", []);
+  angular.module("enviroCar.community", []);
+  angular.module("enviroCar.feedback", []);
+
+  /*
+   * The default legend directive of ui-leaflet is conflicting in the
+   * combination of used library versions of leaflet, leaflet draw and leaflet
+   * heat.
+   * An override of the legend-directive is necessary:
+   */
+  angular.module("ui-leaflet").directive("legend", [
+    "leafletLogger",
+    "$http",
+    "$timeout",
+    "leafletHelpers",
+    "leafletLegendHelpers",
+    function(
+      leafletLogger,
+      $http,
+      $timeout,
+      leafletHelpers,
+      leafletLegendHelpers
+    ) {
+      "ngInject";
+      var $log = leafletLogger,
+        errorHeader = leafletHelpers.errorHeader + " [Legend] ";
+      return {
+        restrict: "A",
+        scope: false,
+        replace: false,
+        require: "leaflet",
+        transclude: false,
+
+        link: function(scope, element, attrs, controller) {
+          var isArray = leafletHelpers.isArray,
+            isString = leafletHelpers.isString,
+            isDefined = leafletHelpers.isDefined,
+            isFunction = leafletHelpers.isFunction,
+            leafletScope = controller.getLeafletScope(),
+            legend = leafletScope.legend;
+
+          var legendClass;
+          var position;
+          var leafletLegend;
+          var type;
+
+          leafletScope.$watch(
+            "legend",
+            function(newLegend) {
+              if (isDefined(newLegend)) {
+                legendClass = newLegend.legendClass
+                  ? newLegend.legendClass
+                  : "legend";
+                position = newLegend.position || "bottomright";
+                // default to arcgis
+                type = newLegend.type || "arcgis";
+              }
+            },
+            true
+          );
+
+          var createLegend = function(map, legendData, newURL) {
+            if (
+              legendData &&
+              legendData.layers &&
+              legendData.layers.length > 0
+            ) {
+              if (isDefined(leafletLegend)) {
+                leafletLegendHelpers.updateLegend(
+                  leafletLegend.getContainer(),
+                  legendData,
+                  type,
+                  newURL
+                );
+              } else {
+                leafletLegend = L.control({
+                  position: position
                 });
+                leafletLegend.onAdd = leafletLegendHelpers.getOnAddLegend(
+                  legendData,
+                  legendClass,
+                  type,
+                  newURL
+                );
+                leafletLegend.addTo(map);
+              }
 
-                $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+              if (
+                isDefined(legend.loadedData) &&
+                isFunction(legend.loadedData)
+              ) {
+                legend.loadedData();
+              }
+            }
+          };
+
+          controller.getMap().then(function(map) {
+            leafletScope.$watch("legend", function(newLegend) {
+              if (!isDefined(newLegend)) {
+                if (isDefined(leafletLegend)) {
+                  leafletLegend.removeFrom(map);
+                  leafletLegend = null;
+                }
+
+                return;
+              }
+
+              if (
+                !isDefined(newLegend.url) &&
+                type === "arcgis" &&
+                (!isArray(newLegend.colors) ||
+                  !isArray(newLegend.labels) ||
+                  newLegend.colors.length !== newLegend.labels.length)
+              ) {
+                $log.warn(
+                  errorHeader + " legend.colors and legend.labels must be set."
+                );
+                return;
+              }
+
+              if (isDefined(newLegend.url)) {
+                $log.info(errorHeader + " loading legend service.");
+                return;
+              }
+
+              if (isDefined(leafletLegend)) {
+                leafletLegend.remove(map);
+                leafletLegend = null;
+              }
+
+              leafletLegend = L.control({
+                position: position
+              });
+
+              if (type === "arcgis") {
+                leafletLegend.onAdd = leafletLegendHelpers.getOnAddArrayLegend(
+                  newLegend,
+                  legendClass
+                );
+              }
+              leafletLegend.addTo(map);
             });
 
+            leafletScope.$watch("legend.url", function(newURL) {
+              if (!isDefined(newURL)) {
+                return;
+              }
 
-    angular.module('enviroCar.api', []);
+              if (!isArray(newURL) && !isString(newURL)) {
+                $log.warn(
+                  errorHeader + " legend.url must be an array or string."
+                );
+                return;
+              }
 
-    angular.module('enviroCar.auth', []);
+              var urls = isString(newURL) ? [newURL] : newURL;
 
-    angular.module('enviroCar.track', []);
+              var legendData;
+              var onResult = function(idx, url) {
+                return function(ld) {
+                  if (isDefined(ld.data.error)) {
+                    $log.warn(
+                      errorHeader + "Error loadin legend from: " + url,
+                      ld.data.error.message
+                    );
+                  } else {
+                    if (
+                      legendData &&
+                      legendData.layers &&
+                      legendData.layers.length > 0
+                    ) {
+                      legendData.layers = legendData.layers.concat(
+                        ld.data.layers
+                      );
+                    } else {
+                      legendData = ld.data;
+                    }
+                  }
 
-    angular.module('enviroCar.tracks', []);
+                  if (idx === urls.length - 1) {
+                    createLegend(map, legendData, newURL);
+                  }
+                };
+              };
+              var onError = function(err) {
+                $log.warn(errorHeader + " legend.url not loaded.", err);
+              };
 
-    angular.module('enviroCar.profile', []);
+              for (var i = 0; i < urls.length; i++) {
+                leafletLegendHelpers
+                  .addLegendURL(attrs.id, {
+                    url: urls[i],
+                    method: "GET"
+                  })
+                  .then(onResult(i))
+                  .catch(onError);
+              }
+            });
 
-    angular.module('enviroCar.community', []);
-    
-    angular.module('enviroCar.feedback', []);
-
-    /*
-     * The default legend directive of ui-leaflet is conflicting in the 
-     * combination of used library versions of leaflet, leaflet draw and leaflet 
-     * heat.
-     * An override of the legend-directive is necessary:
-     */
-    angular.module("ui-leaflet").directive('legend', ["leafletLogger", "$http", "$timeout", "leafletHelpers", "leafletLegendHelpers", function (leafletLogger, $http, $timeout, leafletHelpers, leafletLegendHelpers) {
-            "ngInject";
-            var $log = leafletLogger,
-                    errorHeader = leafletHelpers.errorHeader + ' [Legend] ';
-            return {
-                restrict: "A",
-                scope: false,
-                replace: false,
-                require: 'leaflet',
-                transclude: false,
-
-                link: function (scope, element, attrs, controller) {
-
-                    var isArray = leafletHelpers.isArray,
-                            isString = leafletHelpers.isString,
-                            isDefined = leafletHelpers.isDefined,
-                            isFunction = leafletHelpers.isFunction,
-                            leafletScope = controller.getLeafletScope(),
-                            legend = leafletScope.legend;
-
-                    var legendClass;
-                    var position;
-                    var leafletLegend;
-                    var type;
-
-                    leafletScope.$watch('legend', function (newLegend) {
-
-                        if (isDefined(newLegend)) {
-                            legendClass = newLegend.legendClass ? newLegend.legendClass : "legend";
-                            position = newLegend.position || 'bottomright';
-                            // default to arcgis
-                            type = newLegend.type || 'arcgis';
-                        }
-                    }, true);
-
-                    var createLegend = function (map, legendData, newURL) {
-                        if (legendData && legendData.layers && legendData.layers.length > 0) {
-                            if (isDefined(leafletLegend)) {
-                                leafletLegendHelpers.updateLegend(leafletLegend.getContainer(), legendData, type, newURL);
-                            } else {
-                                leafletLegend = L.control({
-                                    position: position
-                                });
-                                leafletLegend.onAdd = leafletLegendHelpers.getOnAddLegend(legendData, legendClass, type, newURL);
-                                leafletLegend.addTo(map);
-                            }
-
-                            if (isDefined(legend.loadedData) && isFunction(legend.loadedData)) {
-                                legend.loadedData();
-                            }
-                        }
-                    };
-
-                    controller.getMap().then(function (map) {
-                        leafletScope.$watch('legend', function (newLegend) {
-                            if (!isDefined(newLegend)) {
-                                if (isDefined(leafletLegend)) {
-                                    leafletLegend.removeFrom(map);
-                                    leafletLegend = null;
-                                }
-
-                                return;
-                            }
-
-                            if (!isDefined(newLegend.url) && (type === 'arcgis') && (!isArray(newLegend.colors) || !isArray(newLegend.labels) || newLegend.colors.length !== newLegend.labels.length)) {
-                                $log.warn(errorHeader + " legend.colors and legend.labels must be set.");
-                                return;
-                            }
-
-                            if (isDefined(newLegend.url)) {
-                                $log.info(errorHeader + " loading legend service.");
-                                return;
-                            }
-
-                            if (isDefined(leafletLegend)) {
-                                leafletLegend.remove(map);
-                                leafletLegend = null;
-                            }
-
-                            leafletLegend = L.control({
-                                position: position
-                            });
-
-                            if (type === 'arcgis') {
-                                leafletLegend.onAdd = leafletLegendHelpers.getOnAddArrayLegend(newLegend, legendClass);
-                            }
-                            leafletLegend.addTo(map);
-                        });
-
-                        leafletScope.$watch('legend.url', function (newURL) {
-                            if (!isDefined(newURL)) {
-                                return;
-                            }
-
-                            if (!isArray(newURL) && !isString(newURL)) {
-                                $log.warn(errorHeader + " legend.url must be an array or string.");
-                                return;
-                            }
-
-                            var urls = isString(newURL) ? [newURL] : newURL;
-
-                            var legendData;
-                            var onResult = function (idx, url) {
-                                return function (ld) {
-                                    if (isDefined(ld.data.error)) {
-                                        $log.warn(errorHeader + 'Error loadin legend from: ' + url, ld.data.error.message);
-                                    } else {
-                                        if (legendData && legendData.layers && legendData.layers.length > 0) {
-                                            legendData.layers = legendData.layers.concat(ld.data.layers);
-                                        } else {
-                                            legendData = ld.data;
-                                        }
-                                    }
-
-                                    if (idx === urls.length - 1) {
-                                        createLegend(map, legendData, newURL);
-                                    }
-                                };
-                            };
-                            var onError = function (err) {
-                                $log.warn(errorHeader + ' legend.url not loaded.', err);
-                            };
-
-                            for (var i = 0; i < urls.length; i++) {
-                                leafletLegendHelpers.addLegendURL(attrs.id, {
-                                    url: urls[i],
-                                    method: 'GET'
-                                }).then(onResult(i)).catch(onError);
-                            }
-                        });
-
-                        leafletScope.$watch('legend.legendData', function (legendData) {
-                            $log.debug('legendData', legendData);
-                            if (isDefined(leafletScope.legend.url) || !isDefined(legendData)) {
-                                return;
-                            }
-                            createLegend(map, legendData);
-                        }, true);
-                    });
-
+            leafletScope.$watch(
+              "legend.legendData",
+              function(legendData) {
+                $log.debug("legendData", legendData);
+                if (
+                  isDefined(leafletScope.legend.url) ||
+                  !isDefined(legendData)
+                ) {
+                  return;
                 }
-            };
-        }]);
+                createLegend(map, legendData);
+              },
+              true
+            );
+          });
+        }
+      };
+    }
+  ]);
 
-    /**
-     * A decorator $delegate is necessary or otherwise we gonna have two active 
-     * legend directives
-     */
-    angular.module("ui-leaflet").decorator(
-            "legendDirective",
-            function legendDirectiveDecorator($delegate) {
-                "ngInject";
-                console.log(". . . . . . . . . . . . . .");
-                console.log("There are %s matching directives.", $delegate.length);
-                console.log(". . . . . . . . . . . . . .");
-                // make use of only the index[1] legend-directive - which is our
-                //  declaration of the legend directive 
-                var randomDirective = $delegate[ 1 ];
-                // Demonstrating that our custom "label" field is available on the
-                // object in the $delegate collection.
-                console.log(". . . . . . . . . . . . . .");
-                // Return a new array that contains only the randomly-selected version
-                // of the directive.
-                return([randomDirective]);
-            }
-    );
-
+  /**
+   * A decorator $delegate is necessary or otherwise we gonna have two active
+   * legend directives
+   */
+  angular
+    .module("ui-leaflet")
+    .decorator("legendDirective", function legendDirectiveDecorator($delegate) {
+      "ngInject";
+      console.log(". . . . . . . . . . . . . .");
+      console.log("There are %s matching directives.", $delegate.length);
+      console.log(". . . . . . . . . . . . . .");
+      // make use of only the index[1] legend-directive - which is our
+      //  declaration of the legend directive
+      var randomDirective = $delegate[1];
+      // Demonstrating that our custom "label" field is available on the
+      // object in the $delegate collection.
+      console.log(". . . . . . . . . . . . . .");
+      // Return a new array that contains only the randomly-selected version
+      // of the directive.
+      return [randomDirective];
+    });
 })();
--- app/config/config.js
+++ app/config/config.js
@@ -1,113 +1,118 @@
-(function () {
-    'use strict';
+(function() {
+  "use strict";
 
-    function config($stateProvider, $urlRouterProvider, $mdThemingProvider, $httpProvider) {
-        "ngInject";
-        console.log('run config');
-        $stateProvider
-                .state('login', {
-                    url: '/login?username&code&user',
-                    templateUrl: 'app/components/login/login.html',
-                    controller: 'LoginCtrl',
-                    authenticate: false
-                })
-                .state('dashboard', {
-                    url: '/dashboard',
-                    templateUrl: 'app/components/dashboard/dashboard.html',
-                    controller: 'DashboardCtrl',
-                    authenticate: true
-                })
-                .state('chart', {
-                    url: '/dashboard/track/:trackid',
-                    controller: 'ChartCtrl',
-                    templateUrl: function (stateParams) {
-                        return 'app/components/track_analysis/chart/chart.html?path=' + stateParams.trackid;
-                    },
-                    authenticate: true,
-                    data: {
-                        title: 'Chart'
-                    }
-                })
-                .state('tracks', {
-                    url: '/tracks',
-                    controller: 'TrackListCtrl',
-                    templateUrl: 'app/components/tracks/tracks.html',
-                    authenticate: true
-                })
-                .state('profile', {
-                    url: '/profile',
-                    controller: 'ProfileCtrl',
-                    templateUrl: 'app/components/profile/profile.html',
-                    authenticate: true
-                })
-                .state('feedback', {
-                    url: '/feedback',
-                    controller: 'FeedbackCtrl',
-                    templateUrl: 'app/components/feedback/feedback.html',
-                    authenticate: false
-                });
+  function config(
+    $stateProvider,
+    $urlRouterProvider,
+    $mdThemingProvider,
+    $httpProvider
+  ) {
+    "ngInject";
+    console.log("run config");
+    $stateProvider
+      .state("login", {
+        url: "/login?username&code&user",
+        templateUrl: "app/components/login/login.html",
+        controller: "LoginCtrl",
+        authenticate: false
+      })
+      .state("dashboard", {
+        url: "/dashboard",
+        templateUrl: "app/components/dashboard/dashboard.html",
+        controller: "DashboardCtrl",
+        authenticate: true
+      })
+      .state("chart", {
+        url: "/dashboard/track/:trackid",
+        controller: "ChartCtrl",
+        templateUrl: function(stateParams) {
+          return (
+            "app/components/track_analysis/chart/chart.html?path=" +
+            stateParams.trackid
+          );
+        },
+        authenticate: true,
+        data: { title: "Chart" }
+      })
+      .state("tracks", {
+        url: "/tracks",
+        controller: "TrackListCtrl",
+        templateUrl: "app/components/tracks/tracks.html",
+        authenticate: true
+      })
+      .state("profile", {
+        url: "/profile",
+        controller: "ProfileCtrl",
+        templateUrl: "app/components/profile/profile.html",
+        authenticate: true
+      })
+      .state("feedback", {
+        url: "/feedback",
+        controller: "FeedbackCtrl",
+        templateUrl: "app/components/feedback/feedback.html",
+        authenticate: false
+      });
 
-        $urlRouterProvider.otherwise('/dashboard');
+    $urlRouterProvider.otherwise("/dashboard");
 
-        $mdThemingProvider
-                .definePalette('envirocar-blue', {// envirocar.org blue
-                    '50': '1A80C1',
-                    '100': '1A80C1',
-                    '200': '1A80C1',
-                    '300': '1A80C1',
-                    '400': '1A80C1',
-                    '500': '1A80C1',
-                    '600': '1A80C1',
-                    '700': '1A80C1',
-                    '800': '1A80C1',
-                    '900': '1A80C1',
-                    'A100': '1A80C1',
-                    'A200': '1A80C1',
-                    'A400': '1A80C1',
-                    'A700': '1A80C1',
-                    'contrastDefaultColor': 'light',
-                    'contrastDarkColors': ['50', '100',
-                        '200', '300', '400', 'A100'],
-                    'contrastLightColors': undefined
-                })
-                .definePalette('envirocar-green', {// envirocar.org green
-                    '50': '8cbf3f',
-                    '100': '8cbf3f',
-                    '200': '8cbf3f',
-                    '300': '8cbf3f',
-                    '400': '8cbf3f',
-                    '500': '8cbf3f',
-                    '600': '8cbf3f',
-                    '700': '8cbf3f',
-                    '800': '8cbf3f',
-                    '900': '8cbf3f',
-                    'A100': '8cbf3f',
-                    'A200': '8cbf3f',
-                    'A400': '8cbf3f',
-                    'A700': '8cbf3f',
-                    'contrastDefaultColor': 'light',
-                    'contrastDarkColors': ['50', '100',
-                        '200', '300', '400', 'A100'],
-                    'contrastLightColors': undefined
-                })
-                .theme('default')
-                .primaryPalette('envirocar-blue', {
-                    'default': '500'
-                })
-                .accentPalette('envirocar-green', {
-                    'default': '500'
-                })
-                .warnPalette('pink');
+    $mdThemingProvider
+      .definePalette("envirocar-blue", {
+        // envirocar.org blue
+        "50": "1A80C1",
+        "100": "1A80C1",
+        "200": "1A80C1",
+        "300": "1A80C1",
+        "400": "1A80C1",
+        "500": "1A80C1",
+        "600": "1A80C1",
+        "700": "1A80C1",
+        "800": "1A80C1",
+        "900": "1A80C1",
+        A100: "1A80C1",
+        A200: "1A80C1",
+        A400: "1A80C1",
+        A700: "1A80C1",
+        contrastDefaultColor: "light",
+        contrastDarkColors: ["50", "100", "200", "300", "400", "A100"],
+        contrastLightColors: undefined
+      })
+      .definePalette("envirocar-green", {
+        // envirocar.org green
+        "50": "8cbf3f",
+        "100": "8cbf3f",
+        "200": "8cbf3f",
+        "300": "8cbf3f",
+        "400": "8cbf3f",
+        "500": "8cbf3f",
+        "600": "8cbf3f",
+        "700": "8cbf3f",
+        "800": "8cbf3f",
+        "900": "8cbf3f",
+        A100: "8cbf3f",
+        A200: "8cbf3f",
+        A400: "8cbf3f",
+        A700: "8cbf3f",
+        contrastDefaultColor: "light",
+        contrastDarkColors: ["50", "100", "200", "300", "400", "A100"],
+        contrastLightColors: undefined
+      })
+      .theme("default")
+      .primaryPalette("envirocar-blue", {
+        default: "500"
+      })
+      .accentPalette("envirocar-green", {
+        default: "500"
+      })
+      .warnPalette("pink");
 
-        $httpProvider.defaults.headers.common = {};
-        $httpProvider.defaults.headers.post = {};
-        $httpProvider.defaults.headers.put = {};
-        $httpProvider.defaults.headers.patch = {};
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-        $httpProvider.defaults.withCredentials = true;
-    }
+    $httpProvider.defaults.headers.common = {};
+    $httpProvider.defaults.headers.post = {};
+    $httpProvider.defaults.headers.put = {};
+    $httpProvider.defaults.headers.patch = {};
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.withCredentials = true;
+  }
 
-    angular.module('enviroCar')
-            .config(config);
+  angular.module("enviroCar").config(config);
 })();
