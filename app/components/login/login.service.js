bb6cffc2e326d09b23987d48fd71a92138233ccd
Christian Autermann <c.autermann@52north.org>
2019-12-12 11:43:12 +0100

    fix logout

--- app/components/login/login.service.js
+++ app/components/login/login.service.js
@@ -14,43 +14,24 @@
     this.getCredentials = function() {
       return userCredentials;
     };
-    
+
     this.logout = function() {
-      userCredentials.username = "";
       $.ajax({
         type: "POST",
         url: ecBase + "/logout",
-        //                    withCredentials: true,
         xhrFields: { withCredentials: true },
         // log out at server
         success: function(data, status, jqxhr) {
-          $.ajax({
-            type: "GET",
-            url: ecBaseUrl + "/users",
-            crossDomain: true,
-            success: function() {
-              // 401 expected here
-              console.log("logout failed!");
-            },
-            error: function(request) {
-              if (request.status === 401) {
-                $cookies.remove("JSESSIONID");
-                $cookieStore.remove("JSESSIONID");
-                document.cookie.split(";").forEach(function(c) {
-                  document.cookie = c
-                    .replace(/^ +/, "")
-                    .replace(
-                      /=.*/,
-                      "=;expires=" + new Date().toUTCString() + ";path=/"
-                    );
-                });
-                // credentials resetted => redirect
-                //
-                //                                window.location.replace(ecBaseUrl + "/logout-success");
-              } else {
-                console.log("logout failed!");
-              }
-            }
+          userCredentials.username = "";
+          $cookies.remove("JSESSIONID");
+          $cookieStore.remove("JSESSIONID");
+          document.cookie.split(";").forEach(function(c) {
+            document.cookie = c
+              .replace(/^ +/, "")
+              .replace(
+                /=.*/,
+                "=;expires=" + new Date().toUTCString() + ";path=/"
+              );
           });
         }
       });
