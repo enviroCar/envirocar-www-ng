f68661ae754c2055fa57335a5ba2333869bc456d
Sebastian Drost <s.drost@52north.org>
2021-02-26 17:46:21 +0100

    Add proper translation for login labels

--- app/components/translationProvider.js
+++ app/components/translationProvider.js
@@ -52,7 +52,7 @@ angular
         USERNAME: "Username",
         PASSWORD: "Password",
         REGISTER: "Register",
-        LOGIN: "my enviroCar",
+        LOGIN: "Login",
         FORGOT_PASSWORD: "forgot password?",
         INVALID_LOGIN: "Invalid user credentials.",
         PASSWORD_REPEAT: "repeat the password",
